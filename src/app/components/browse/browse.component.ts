9d9d95e89560781939536f0f342bea3af381db55
SUMIT RIDHAL <sumitridhal@gmail.com>
2017-07-26 03:03:07 +0530

    Card Hover Button

--- src/app/components/browse/browse.component.ts
+++ src/app/components/browse/browse.component.ts
@@ -6,8 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./browse.component.css']
 })
 export class BrowseComponent implements OnInit {
+  visibility: string = 'transition hidden';
+  dimmed: string = '';
+  duration: string = '500ms';
 
   constructor() { }
+
+  changeStyle($event) {
+    if ($event.type == 'mouseover') {
+      this.visibility = 'animating fade in';
+      this.dimmed = 'dimmed';
+      setTimeout(function() {
+        this.visibility = 'visible active';
+      }, 200);
+    }
+    else {
+      this.visibility = 'animating fade out';
+      this.dimmed = '';
+      setTimeout(function() {
+        this.visibility = 'transition hidden';
+      }, 200);
+
+    }
+
+  }
+
 
   ngOnInit() {
   }
